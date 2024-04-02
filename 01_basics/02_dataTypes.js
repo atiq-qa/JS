077371297cc540c1e941081ac54d51f850bf4ef3
Admin <atiqniazai@gmail.com>
2024-04-02 18:14:39 -0400

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// number => 2 to the power of 53
+// bigint
+// string => "", '', prefer to use ""
+// boolean => true/ false
+// null => standalone value
+// undefined => its itself a data type
+// symbol => unique
+
+
+//object
+
+console.log(typeof 2)           //data type is number
+console.log(typeof "null")      //data type is string
+console.log(typeof null)        //data type is object
+console.log(typeof Symbol)      //data type is function
+console.log(typeof undefined)   //data type is undefined 
+
