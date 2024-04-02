0daba744ef54a0636fb0d6f9a1f1d3081541c654
Admin <atiqniazai@gmail.com>
2024-04-02 18:14:18 -0400

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "atiq@gmail.com"
+var accountPassword = "pass123"
+accountCity = "Bihar"
+let accountState
+
+
+// accountId = 234455   // constant variables cannot be modified
+
+accountemail = "jalal@gmail.com"    //variables are case sensitive
+accountPassword = "1234Pass"
+
+console.log(accountEmail)
+console.log(accountPassword)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
