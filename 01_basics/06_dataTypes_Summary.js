a258f60f9369d31f526b849253607b2a339d9699
Admin <atiqniazai@gmail.com>
2024-04-03 16:20:51 -0400

    Data type summary1

---
+++ 01_basics/06_dataTypes_Summary.js
@@ -0,0 +1,14 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol("123")
+const sym2 = Symbol("foo");
+
+console.log(sym2)
+//Reference (Non Primitive)
+//3 Types: Array, Objects, Funcitons
