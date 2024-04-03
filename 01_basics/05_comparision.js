e2ee7a3cb3147a70d20e5c850c13bf5a4d864ef3
Admin <atiqniazai@gmail.com>
2024-04-03 15:57:20 -0400

    comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,28 @@
+/* *********** Same Data type comparaision ****************** */
+
+//console.log(2 > 1)
+//console.log(2 > 5)
+//console.log(2 > 2)
+//console.log(2 <= 1)
+//console.log(2 != 1)
+//console.log(2 == 1)
+//console.log(2 == 2)
+
+/* ************** Different Data Types comparision ************ */
+
+//console.log("2" > 1)
+//console.log("2" == 2)
+//console.log("2" >= 1)
+//console.log("2" <= 1)
+
+console.log(null > 0)	//null value is not pre defined.
+console.log(null == 0)
+console.log(null === 0)
+console.log(null >= 0)
+
+
+console.log(undefined > 0)	//undefined value is not pre defined.
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+// difference between == and === the === check the data type as well and then do the comparision its called strict check.
