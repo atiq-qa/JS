cb82d205304605678b5f5fca630f211dffed7976
Admin <atiqniazai@gmail.com>
2024-04-03 15:34:06 -0400

    Operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+
+//console.log(value)
+//console.log(negValue)
+
+//console.log("Addition: "+ (2 + 2))
+//console.log("subtraction: "+ (2 - 2))
+//console.log("Multiplication: "+2 * 2)
+//console.log("Division: "+2 / 2)
+//console.log("Remainder: "+2 % 2)
+//console.log("Power: "+2 ** 3)
+
+//************************ String Operations ********************************/
+
+//let str1 = "Hello"
+//let str2 = " World"
+//let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2)    
+//console.log(1 + "2")
+//console.log("1" + 2 + 2)
+//console.log(1 + 2 + "2")
+
+
+let gameCounter1 = 100
+console.log(gameCounter1++)
+let gameCounter2 = 100
+console.log(++gameCounter2)
+
+    /* ******************** Notes ************************
+    "1" + 1, "1" + 2 + 2 => if first operand is string all remaining operands will be treated as strings
+    1 + 2 + "2" => if the first operand is number, they will be treated as number and then concatanate with the string.
+    value++     => the value will be used first and then increment by one.
+    ++value     => the value will be increment first and then used
+    */
