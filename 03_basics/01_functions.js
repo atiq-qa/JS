28caf0d66522832a5bfb2307ea5581f6eb036c6f
Admin <atiqniazai@gmail.com>
2024-04-25 14:52:28 -0400

    functions basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,65 @@
+function sayMyName() {
+    console.log("A")
+    console.log("T")
+    console.log("I")
+    console.log("Q")
+
+}
+
+
+//sayMyName()
+
+//function addTowNumbers(number1, number2) {
+//    console.log( number1  + number2)
+//}
+
+//function addTowNumbers1(number1, number2) {
+//    let result = number1 + number2
+//    return result
+//}
+
+//let result = addTowNumbers1(2, 6)
+//console.log(result)
+
+//function loginUserMessage(username) {
+//    console.log( '${username} just logged in')
+//}
+
+//loginUserMessage("Atiq")
+
+
+//function calculateCartPrice(num1) {
+//    return num1
+//}
+
+//function calculateCartPrice(...num1) {
+//    return num1
+//}
+
+
+//function calculateCartPrice(val1, val2, ...num1) {
+//    return num1
+//}
+//console.log(calculateCartPrice(200,400,300,400,500))
+
+//const course = {
+//    name: "Java",
+//    price: 199
+
+//}
+
+//function handleObject(input) {
+//    console.log(`The course name is ${input.name} and price is only ${input.price}`)
+//}
+
+//handleObject({name: "Java", price: 299})
+
+const newArary = [100, 200, 300, 400]
+
+function handleArray(inputArray) {
+    return inputArray[2]
+}
+
+//console.log(handleArray(newArary))
+
+console.log(handleArray([300,500,600,700]))
